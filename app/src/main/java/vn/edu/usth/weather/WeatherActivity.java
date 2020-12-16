39b9667d7f96262dfac64ac5a38cc3045026920b
Minh Long <migloh_@users.noreply.github.com>
2020-12-16 16:37:26 +0700

    Labwork 07

--- app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,6 +1,10 @@
 package vn.edu.usth.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -10,13 +14,17 @@ public class WeatherActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        ForecastFragment firstFragment = new ForecastFragment();
+//        ForecastFragment firstFragment = new ForecastFragment();
 //        uppercloud kumoriFragment = new uppercloud();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
         Toast.makeText(getApplicationContext(), "onCreate()", Toast.LENGTH_SHORT).show();
-        getSupportFragmentManager().beginTransaction().replace(R.id.container_,firstFragment).commit();
+//        getSupportFragmentManager().beginTransaction().replace(R.id.container_,firstFragment).commit();
 //        getSupportFragmentManager().beginTransaction().replace(R.id.kumori, kumoriFragment).commit();
+        PagerAdapter adapter = new HomePagerAdapter(getSupportFragmentManager());
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        pager.setOffscreenPageLimit(3);
+        pager.setAdapter(adapter);
     }
 
     protected void onStart() {
@@ -47,5 +55,32 @@ public class WeatherActivity extends AppCompatActivity {
         super.onDestroy();
         Log.i("Tenki", "onDestroy()");
         Toast.makeText(getApplicationContext(), "onDestroy()", Toast.LENGTH_LONG).show();
+    }
+
+    public class HomePagerAdapter extends FragmentPagerAdapter {
+        private String titles[] = new String[] {"Hanoi", "Paris", "Toulouse"};
+        public HomePagerAdapter(FragmentManager fm){
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public WeatherAndForecastFragment getItem(int page) {
+//            switch (page) {
+//                case 0: return WeatherAndForecastFragment.newInstance();
+//                case 1: return WeatherAndForecastFragment.newInstance();
+//                case 2: return WeatherAndForecastFragment.newInstance();
+//            }
+            return WeatherAndForecastFragment.newInstance();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int page) {
+            return titles[page];
+        }
     }
 }
